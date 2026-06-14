dfa36aec2762da867fdb3c0083e76a271a81d035
Zhengrong Wang <seanyukigeek@gmail.com>
2026-06-15 00:32:08 +0800

    Return hardware SM id for PTX smid register

--- src/cuda-sim/ptx_sim.cc
+++ src/cuda-sim/ptx_sim.cc
@@ -300,8 +300,7 @@ unsigned ptx_thread_info::get_builtin(int builtin_id, unsigned dim_mod) {
       feature_not_implemented("%pm");
       return 0;
     case SMID_REG:
-      feature_not_implemented("%smid");
-      return 0;
+      return m_hw_sid;
     case TID_REG:
       assert(dim_mod < 3);
       if (dim_mod == 0) return m_tid.x;
